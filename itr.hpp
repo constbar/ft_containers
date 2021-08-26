df8e0d13e2653e0da8b9ca370d49a3377eeb5341
agroup <const.bar@mail.ru>
2021-08-26 23:38:40 +0300

    d2

--- itr.hpp
+++ itr.hpp
@@ -1,34 +1,57 @@
 #ifndef ITR_HPP
 #define ITR_HPP
 
+#include <iterator>
+
+
+// diy aka nvm: Nevermind. // idk i dont know
 namespace my {
 
-	template <typename T>
-	class iter {
+	// template <class Category, class T, class Distance = ptrdiff_t,
+			//   class Pointer = T *, class Reference = T &>
+	template <typename T, typename pointer = T*, typename reference = T&> // next string
+	//  : public std::iterator<std::random_access_iterator_tag, T>
+	class iter
+	{ // rename to ra_iter
+	
+	private:
+		pointer ptr_iter; // rename
 
-		public:
+	public:
+		// typedef T *pointer;
+		// typedef T &reference;
 
-			typedef T* pointer;
-			typedef T& reference;
+		iter() : ptr_iter(NULL) { // speed of exec
+			std::cout << "ptr null const" << std::endl; }
 
-			iter() : ptr_iter(NULL) {
-				std::cout << "ptr null const" << std::endl; }
+		iter(pointer input) {
+			this->ptr_iter = input;
+			std::cout << "ptr const with arg" << std::endl; }
 
-			iter(pointer first) : ptr_iter(first) {
-				std::cout << "ptr const with arg" << std::endl; }
+		iter(const iter &other) {
+			this->ptr_iter = other.ptr_iter;
+			std::cout << "copy constr" << std::endl; }
 
-			iter(const iter &op) : ptr_iter(op.ptr_iter) {
-				std::cout << "copy constt" << std::endl; }
+		iter &operator=(const iter &other) {
+			if (this == &other) return *this;
+			this->ptr_iter = other.ptr_iter;
+			return *this; }
 
-			reference operator*() const {
-				return *this->ptr_iter; }
+		~iter() {} // virtual
 
-		private:
-			// T* ptr_iter;
-			pointer ptr_iter;
-		
+		reference operator*() const {
+			std::cout << "ref in work" << std::endl;
+			return *this->ptr_iter; }
 
 	};
 }
 
 #endif
+
+// template <typename _Iterator>
+// class reverse_iterator
+	// : public iterator<typename iterator_traits<_Iterator>::iterator_category,
+	// 				  typename iterator_traits<_Iterator>::value_type,
+	// 				  typename iterator_traits<_Iterator>::difference_type,
+	// 				  typename iterator_traits<_Iterator>::pointer,
+	// 				  typename iterator_traits<_Iterator>::reference>
--- main.cpp
+++ main.cpp
@@ -1,30 +1,105 @@
 #include <iostream>
+#include <iterator>
 #include <vector>
 
 #include "vec.hpp"
 #include "itr.hpp"
 
+template <typename Iterator>
+void FooImpl(Iterator first, Iterator last, std::random_access_iterator_tag)
+{
+
+}
+template <typename Iterator>
+void Foo(Iterator first, Iterator last)
+{
+	typedef typename std::iterator_traits<Iterator>::iterator_category category;
+	return FooImpl(first, last, category());
+}
+
 int main() {
 
 	// // this is vector 
-	// std::vector<int> kek;
-	// kek.push_back(1);
-	// std::vector<int>::iterator it;
-	// it = kek.begin();
-
-	int i = 122;
-	int *pint = &i;
-	my::iter<int> p(pint);
-	// p = &i;
-	// *p = 11;
-	// std::cout << i << std::endl;
-	std::cout << *p << std::endl;
-
-
-
-
+	std::vector<int> kek;
+	kek.push_back(1);
+	std::vector<int>::iterator it;
+	it = kek.begin();
 
 #if 0
+	int i = 77;
+	// std::vector<int> arr;
+	my::iter<int> kek;
+	kek = &i;
+	std::cout << *kek << std::endl;
+
+	// std::<int>::iterator it;
+
+	// int *pint = &i;
+
+	// kek = &i;
+	// p = &i;
+	// *kek = 11;
+	// std::cout << i << std::endl;
+	// std::cout << *kek << std::endl;
 #endif
+
+	// std::vector<int> kek;
+	// kek.push_back(2);
+	// kek.push_back(1);
+	// kek.push_back(3);
+	
+	// std::vector<int>::iterator i1 = kek.begin();
+	// std::vector<int>::iterator i2 = kek.end();
 	return 0;
 }
+
+// // std::iterator example
+// #include <iostream> // std::cout
+// #include <iterator> // std::iterator, std::input_iterator_tag
+
+// class MyIterator //: public std::iterator<std::input_iterator_tag, int>
+// {
+// 	int* p;
+// 	public:
+// 		MyIterator(int* x) :p(x) {}
+// 		MyIterator(const MyIterator& mit) : p(mit.p) {}
+// 		MyIterator& operator++() {++p;return *this;}
+// 		MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
+// 		bool operator==(const MyIterator& rhs) const {return p==rhs.p;}
+// 		bool operator!=(const MyIterator& rhs) const {return p!=rhs.p;}
+// 		int& operator*() {return *p;}
+// };
+
+// int main () {
+// 	int numbers[]={10,20,30,40,50};
+// 	MyIterator from(numbers);
+// 	MyIterator until(numbers+5);
+// 	for (MyIterator it=from; it!=until; it++)
+// 		std::cout << *it << ' ';
+// 	std::cout << '\n';
+
+//   return 0;
+// // }
+
+
+// // iterator_traits example
+// #include <iostream> // std::cout
+// #include <iterator> // std::iterator_traits
+// #include <typeinfo> // typeid
+
+// int main()
+// {
+// 	// typedef std::iterator_traits<std::string*> traits;
+// 	// if (typeid(traits::iterator_category) == 
+// 	// typeid(std::random_access_iterator_tag))
+// 	// 	std::cout << "int* is a random-access iterator\n";
+
+// 	// Derived *pd = new Derived;
+// 	// Base *pb = pd;
+// 	// cout << typeid(pb).name() << endl;	//prints "class Base *"
+// 	// cout << typeid(*pb).name() << endl; //prints "class Derived"
+// 	// cout << typeid(pd).name() << endl;	//prints "class Derived *"
+// 	// my::iter<int> kke;
+// 	// std::cout << typeid('1').name() << std::endl; //prints "class Derived"
+// 	return 0;
+// }
