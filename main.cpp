3239514d1e6dda383dea7e3b3c2fa27e39ea8d62
agroup <agroup@student.21-school.ru>
2021-09-29 18:54:12 +0300

    fin

--- main.cpp
+++ main.cpp
@@ -14,23 +14,23 @@
 
 int main() {
 
-	// test_vector test_vec;
-	// test_vec.test_const();
-	// test_vec.test_copy();
-	// test_vec.test_iters();
-	// test_vec.test_first();
-	// test_vec.test_second();
-	// test_vec.test_third();
-	// test_vec.test_operators();
+	test_vector test_vec;
+	test_vec.test_const();
+	test_vec.test_copy();
+	test_vec.test_iters();
+	test_vec.test_first();
+	test_vec.test_second();
+	test_vec.test_third();
+	test_vec.test_operators();
 
-	// test_stack test_stk;
-	// test_stk.test1();
-	// test_stk.test2();
+	test_stack test_stk;
+	test_stk.test1();
+	test_stk.test2();
 
-	// test_map test_m;
-	// test_m.test_const();
+	test_map test_m;
+	test_m.test_const();
 
-	// test_set test_s;
-	// test_s.test();
+	test_set test_s;
+	test_s.test();
 	
 }
